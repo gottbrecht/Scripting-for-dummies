8b9432ce72b4199c1ab79b3a36ec2af7fe007577
Lars Kruse <lakruzz@inc-inc.dk>
2023-03-13 11:57:49 +0000

    Simone og Lars fixede lige det hele

--- scripts/scriptWithDates.sh
+++ scripts/scriptWithDates.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 # * * * * * ~/GitHub/Scripting-for-dummies/scripts/scriptWithDates.sh
 
+# can you show me an if statement I can use in a bash script that check wether or not xmlstarlet is installed and ready to use?
+if command -v xmlstarlet >/dev/null 2>&1 ; then
+  echo "xmlstarlet is installed"
+else
+echo "xmlstarlet is not installed"
+echo installing xmlstarlet
+  sudo apt-get update
+  sudo apt-get install xmlstarlet
+fi
+
+
+
 
 # filename/path defineres, der hvor vores news headlines gemmes i en tekstfil med dato/timestamp
 filename=$(git rev-parse --show-toplevel)/output/News_$(date +"%Y%m%d_%H%M").txt
