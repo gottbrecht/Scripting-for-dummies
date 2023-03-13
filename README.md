656054fa29d6b3b8d35935c07ec1ac840968ec9e
paul7299 <113167556+paul7299@users.noreply.github.com>
2023-03-13 10:46:25 +0000

    readme oprettet

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Scripting-for-dummies
 
+Scriptet 'scriptWithDates' henter nyhedsoverskrifter fra Ekstrabladet via deres RSS-feed og samler dem i en .txt-fil.
+
+1) Scriptet kræver XMLStarlet, og skal derfor være installeret på computeren der kører scriptet. Brug commands:
+
 sudo apt-get update
 sudo apt-get install xmlstarlet
+
+2) Tjek at det er installeret rigtig med command:
+xmlstarlet --version
+
+
+3) Hvis du skal køre runEveryMinute scriptet, skal du sørge for at cron er installeret:
+sudo apt-get update
+sudo apt-get install cron
--- scripts/scriptWithDates.sh
+++ scripts/scriptWithDates.sh
@@ -2,9 +2,10 @@
 # * * * * * ~/GitHub/Scripting-for-dummies/scripts/scriptWithDates.sh
 
 
+# filename/path defineres, der hvor vores news headlines gemmes i en tekstfil med dato/timestamp
 filename=$(git rev-parse --show-toplevel)/output/News_$(date +"%Y%m%d_%H%M").txt
-echo $filename
-date=$(date +"%Y%m%d_%H%M")
+
+
+# Vi kører curl commanden, piper det igennem XMLStarlet for at processere XML filen og gemmer det i $filename som en .txt fil
 curl https://ekstrabladet.dk/rssfeed/nyheder/ | xmlstarlet sel -t -m '//item/title' -v . -n >> $filename
 
-# curl -s https://ekstrabladet.dk/rssfeed/nyheder/ | xmlstarlet sel -t -m '//item/title' -v . -n >> $( git rev-parse --show-toplevel)/output/News_$(date +"%H:%M_%d_%m_%Y").txt
